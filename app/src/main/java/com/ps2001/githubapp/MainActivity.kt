db452ea0fae50d60df463df7c7f81563abe4008b
ptsuardana01 <ptsuardana01@gmail.com>
2023-04-01 01:36:40 +0800

    Add menu search and function to search user

--- app/src/main/java/com/ps2001/githubapp/MainActivity.kt
+++ app/src/main/java/com/ps2001/githubapp/MainActivity.kt
@@ -1,9 +1,13 @@
 package com.ps2001.githubapp
 
+import android.app.SearchManager
+import android.content.Context
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import android.view.Menu
 import android.view.View
+import android.widget.Toast
 import androidx.recyclerview.widget.DividerItemDecoration
 import androidx.recyclerview.widget.LinearLayoutManager
 import com.ps2001.githubapp.adapter.MainAdapter
@@ -15,6 +19,9 @@ import com.ps2001.githubapp.response.ItemsItem
 import retrofit2.Call
 import retrofit2.Callback
 import retrofit2.Response
+import androidx.appcompat.widget.SearchView
+import androidx.lifecycle.ViewModelProvider
+import com.ps2001.githubapp.model.MainViewModel
 
 class MainActivity : AppCompatActivity() {
 
@@ -30,32 +37,67 @@ class MainActivity : AppCompatActivity() {
         binding.rvMain.setHasFixedSize(true)
         showRecyclerView()
 
-        getListUser()
+//        getListUser()
+        val mainViewModel = ViewModelProvider(this, ViewModelProvider.NewInstanceFactory())[MainViewModel::class.java]
+        mainViewModel.vmListUser.observe(this) {
+            setUser(it)
+        }
+
+        mainViewModel.isLoading.observe(this) {
+            showLoading(it)
+        }
     }
 
-    private fun getListUser() {
-        showLoading(true)
-        val client = ApiConfig.getApiService().getUserGithub(QUERY_USER)
-        client.enqueue(object : Callback<GithubResponse> {
-            override fun onResponse(
-                call: Call<GithubResponse>,
-                response: Response<GithubResponse>
-            ) {
-                showLoading(false)
-                if (response.isSuccessful) {
-                    val responseBody = response.body()
-                    if (responseBody != null) {
-                        setUser(responseBody.items)
+    override fun onCreateOptionsMenu(menu: Menu): Boolean {
+        val inflater = menuInflater
+        inflater.inflate(R.menu.search_user, menu)
+
+        val searchManager = getSystemService(Context.SEARCH_SERVICE) as SearchManager
+        val searchView = menu.findItem(R.id.search).actionView as SearchView
+
+        searchView.setSearchableInfo(searchManager.getSearchableInfo(componentName))
+        searchView.queryHint = resources.getString(R.string.search_hint)
+        searchView.setOnQueryTextListener(object : SearchView.OnQueryTextListener {
+            /*
+            Gunakan method ini ketika search selesai atau OK
+             */
+            override fun onQueryTextSubmit(query: String): Boolean {
+                showLoading(true)
+                val client = ApiConfig.getApiService().getUserGithub(query)
+                client.enqueue(object : Callback<GithubResponse> {
+                    override fun onResponse(
+                        call: Call<GithubResponse>,
+                        response: Response<GithubResponse>,
+                    ) {
+                        showLoading(false)
+                        if (response.isSuccessful) {
+                            val responseBody = response.body()
+                            if (responseBody != null) setUser(responseBody.items)
+                        } else {
+                            Log.e("Search", "onFailure: ${response.message()}")
+                            Toast.makeText(this@MainActivity, "Data gagal untuk diproses", Toast.LENGTH_SHORT).show()
+                        }
                     }
-                } else {
-                    Log.e(TAG, "onFailure: ${response.message()}")
-                }
+
+                    override fun onFailure(call: Call<GithubResponse>, t: Throwable) {
+                        showLoading(false)
+                        Log.e("Search", "onFailure: ${t.message}")
+                        Toast.makeText(this@MainActivity, "${t.message}", Toast.LENGTH_SHORT).show()
+                    }
+
+                })
+                searchView.clearFocus()
+                return true
             }
-            override fun onFailure(call: Call<GithubResponse>, t: Throwable) {
-                showLoading(false)
-                Log.e(TAG, "onFailure: ${t.message}")
+
+            /*
+            Gunakan method ini untuk merespon tiap perubahan huruf pada searchView
+             */
+            override fun onQueryTextChange(newText: String): Boolean {
+                return false
             }
         })
+        return true
     }
 
     private fun setUser(items: List<ItemsItem>) {
@@ -84,8 +126,5 @@ class MainActivity : AppCompatActivity() {
         binding.rvMain.addItemDecoration(itemDecoration)
     }
 
-    companion object {
-        private const val TAG = "MainActivity"
-        private const val QUERY_USER = "tutur"
-    }
+    companion object {}
 }
--- app/src/main/java/com/ps2001/githubapp/model/MainViewModel.kt
+++ app/src/main/java/com/ps2001/githubapp/model/MainViewModel.kt
@@ -1,4 +1,83 @@
 package com.ps2001.githubapp.model
 
-class MainViewModel {
+import android.util.Log
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+import com.ps2001.githubapp.api.ApiConfig
+import com.ps2001.githubapp.response.GithubResponse
+import com.ps2001.githubapp.response.ItemsItem
+import retrofit2.Call
+import retrofit2.Callback
+import retrofit2.Response
+
+class MainViewModel : ViewModel() {
+
+    private val _vmListUser = MutableLiveData<List<ItemsItem>>()
+    val vmListUser : LiveData<List<ItemsItem>> = _vmListUser
+
+    private val _isLoading = MutableLiveData<Boolean>()
+    val isLoading : LiveData<Boolean> = _isLoading
+
+    init {
+        getListUser()
+    }
+
+    private fun getListUser() {
+        _isLoading.value = true
+        val client = ApiConfig.getApiService().getUserGithub(QUERY_USER)
+        client.enqueue(object : Callback<GithubResponse> {
+            override fun onResponse(
+                call: Call<GithubResponse>,
+                response: Response<GithubResponse>
+            ) {
+                _isLoading.value = false
+                if (response.isSuccessful) {
+                    val responseBody = response.body()
+                    if (responseBody != null) {
+                        _vmListUser.value = response.body()?.items
+                    }
+                } else {
+                    Log.e(TAG, "onFailure: ${response.message()}")
+                }
+            }
+            override fun onFailure(call: Call<GithubResponse>, t: Throwable) {
+                _isLoading.value = false
+                Log.e(TAG, "onFailure: ${t.message}")
+            }
+        })
+    }
+
+//    private fun getSearchUser(query : String) {
+//        _isLoading.value = true
+//        val client = ApiConfig.getApiService().getUserGithub(query)
+//        client.enqueue(object : Callback<GithubResponse> {
+//            override fun onResponse(
+//                call: Call<GithubResponse>,
+//                response: Response<GithubResponse>,
+//            ) {
+//                _isLoading.value = false
+//                if (response.isSuccessful) {
+//                    val responseBody = response.body()
+//                    if (responseBody != null) {
+//                        _vmListUser.value = response.body()?.items
+//                    }
+//                } else {
+//                    Log.e(TAG, "onFailure: ${response.message()}")
+//                }
+//            }
+//
+//            override fun onFailure(call: Call<GithubResponse>, t: Throwable) {
+//                _isLoading.value = false
+//                Log.e(TAG, "onFailure: ${t.message}")
+//            }
+//
+//        })
+//    }
+
+    companion object {
+        private const val TAG = "Main_VM"
+        private const val QUERY_USER = "arif"
+    }
+
 }
